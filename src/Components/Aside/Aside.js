dbe8bbd976a0009d057d87a954a2dffa879ef89d
Oluyoh okiemute <OLUYOHOKIEMUTE@GMAIL.COM>
2022-10-10 00:41:39 +0100

    almost done with res

--- src/Components/Aside/Aside.js
+++ src/Components/Aside/Aside.js
@@ -3,20 +3,33 @@ import './Aside.css'
 
 const Aside = () => {
 
+    const handleNavBar = () => {
+        let aside_container = document.querySelector('.aside_container')
+        
+        if(aside_container.classList.contains('active')){
+            aside_container.classList.remove('active')
+        }else{
+            aside_container.classList.add('active')
+        }
+    }
+
     return(
         <div className='aside'>
-            <div className='aside_container'>
+            <div className='aside_container active'>
                 <h1>Weather.co</h1>
                 <div className='navitem_container'>
                     <ul>
-                        <li className='navitem active'><i class="fa fa-th-large" aria-hidden="true"></i>Dashboard <span></span></li>
-                        <li className='navitem'><i class="fa fa-globe" aria-hidden="true"></i>Map <span></span></li>
-                        <li className='navitem'><i class="fa fa-heart-o" aria-hidden="true"></i>Saved location <span></span></li>
-                        <li className='navitem'><i class="fa fa-calendar" aria-hidden="true"></i>Calendar <span></span></li>
+                        <li className='navitem active'><i class="fa fa-th-large" aria-hidden="true"></i><p>Dashboard</p> <span></span></li>
+                        <li className='navitem'><i class="fa fa-globe" aria-hidden="true"></i><p>Map</p>  <span></span></li>
+                        <li className='navitem'><i class="fa fa-heart-o" aria-hidden="true"></i><p>Saved location</p> <span></span></li>
+                        <li className='navitem'><i class="fa fa-calendar" aria-hidden="true"></i><p>Calendar</p> <span></span></li>
                     </ul>
                     <ul>
-                        <li className='navitem'><i class="fa fa-sign-out" aria-hidden="true"></i>Sign out</li>
+                        <li className='navitem'><i class="fa fa-sign-out" aria-hidden="true"></i><p>Sign out</p></li>
                     </ul>
+                </div>
+                <div className='aside_toggler' onClick={handleNavBar}>
+                    <i class="fa fa-arrow-right" aria-hidden="true"></i>
                 </div>
             </div>
         </div>
